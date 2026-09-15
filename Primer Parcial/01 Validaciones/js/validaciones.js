b3f4bbbb0fa6f224676e8ebe934f7125f1602cac
ErickDB4190 <ediazb2402@alumno.ipn.mx>
2026-09-15 17:27:41 -0600

    Final por ahora

--- Primer Parcial/01 Validaciones/js/validaciones.js
+++ Primer Parcial/01 Validaciones/js/validaciones.js
@@ -1,13 +1,14 @@
+
 /*Las validaciones para este formulario se realizaran mediante el uso de Expresiones Regulares, Las cuales las vamos a dividir en 3:
 1.- Texto para el nombre
 2.- Numerico para la boleta
 3.- Debe tener un patron para la fecha
 
-Las expresiones regulares, son patrones que nos ayudar a validar cadenas bajo ciertas condiciones.*/ 
+Las expresiones regulares, son patrones que nos ayudar a validar cadenas bajo ciertas condiciones.*/
 
 const patrones = {
 
-    nombre : /^[A-Za-zÁÉÍÓÚÑáéíóúñÜü]\{2,60}$/,
+    nombre : /^[A-Za-zÁÉÍÓÚÑáéíóúñÜü\s]{2,60}$/,
     boleta : /^\d{10}$/,
     fecha : /^(0[1-9]|[12]\d|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/
 }
@@ -25,24 +26,33 @@ function validarCampo(campo, valor){
 
 //para validar el formulario debemos ocupar los principios de obtencion y manipulacion de los elementos del DOM
 if(typeof document != 'undefined') {
-    const formulario =
-    document.getElementById('form-registro');
+
+    const formulario = document.getElementById('form-registro');
 
     formulario.addEventListener('submit', (evento) => {
+
         evento.preventDefault();
+
         let formularioValido = true;
 
         for(const campo of Object.keys(patrones)){
+
             const input = document.getElementById(campo);
             const errorSpan = document.getElementById(`error-${campo}`);
             const esValido = validarCampo(campo, input.value);
+
             input.classList.toggle('invalido', !esValido);
-            errorSpan.textContent = esValido ? '' : mensajes [campo];
-            if (!esValido) formularioValido = false;
+
+            errorSpan.textContent = esValido ? '' : mensajes[campo];
+
+            if (!esValido) {
+                formularioValido = false;
+            }
         }
 
-        if(!esValido){}
         const mensajeExito = document.getElementById('mensaje-exito');
+
         mensajeExito.textContent = formularioValido ? 'Registro exitoso!' : '';
     });
 }
+
