8b6187f995340da0773eea949df1b46ef4dfeb4d
ErickDB4190 <ediazb2402@alumno.ipn.mx>
2026-09-15 17:09:37 -0600

    Agregando el js

--- Primer Parcial/01 Validaciones/js/validaciones.js
+++ Primer Parcial/01 Validaciones/js/validaciones.js
@@ -7,8 +7,8 @@ Las expresiones regulares, son patrones que nos ayudar a validar cadenas bajo ci
 
 const patrones = {
 
-    nombre : /^[A-Za-zÁÉÍÓÚÑáéíóúñÜü]\{2,60}+$/,
-    boleta : /^\{10}$/,
+    nombre : /^[A-Za-zÁÉÍÓÚÑáéíóúñÜü]\{2,60}$/,
+    boleta : /^\d{10}$/,
     fecha : /^(0[1-9]|[12]\d|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/
 }
 
@@ -33,11 +33,11 @@ if(typeof document != 'undefined') {
         let formularioValido = true;
 
         for(const campo of Object.keys(patrones)){
-            const input = document-getElementById(campo);
-            const errorSpan = document.getElementById('error-${campo}');
+            const input = document.getElementById(campo);
+            const errorSpan = document.getElementById(`error-${campo}`);
             const esValido = validarCampo(campo, input.value);
             input.classList.toggle('invalido', !esValido);
-            spanError.textContent = esValido ? '' : mensajes [campo];
+            errorSpan.textContent = esValido ? '' : mensajes [campo];
             if (!esValido) formularioValido = false;
         }
 
