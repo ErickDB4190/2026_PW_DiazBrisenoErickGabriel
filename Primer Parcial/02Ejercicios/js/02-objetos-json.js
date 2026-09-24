1eea1332f793b42ff8c871aeae863b85cdb85253
ErickDB4190 <ediazb2402@alumno.ipn.mx>
2026-09-24 17:31:02 -0600

    Agregando cosas al 02-objetos-json.js

--- Primer Parcial/02Ejercicios/js/02-objetos-json.js
+++ Primer Parcial/02Ejercicios/js/02-objetos-json.js
@@ -4,17 +4,30 @@
 const taller = {
   nombre: 'Introducción a Python',
   instructor: 'Ing. María López',
-  cupo: 25,
+  cupo: 25, 
   inscritos: 25,
 };
 
 // TODO: Object.keys — imprime solo los nombres de las propiedades de `taller`
-
+console.log('Manejo de llaves');
+console.log(Object.keys(taller));
 // TODO: Object.values — imprime solo los valores
-
+console.log('manejo de valores en un objeto');
+console.log(Object.values(taller));
 // TODO: Object.entries — recorre con for..of e imprime "campo: valor" de cada propiedad
-
+console.log('Manejo de propiedades de un objeto');
+for(const [campo, valor] of Object.entries(taller)){
+  console.log(`${campo}: ${valor}`);
+}
 // TODO: JSON.stringify — convierte `taller` a texto (guárdalo en `textoJson`) e imprímelo
-
+console.log('Transformacion de JSON a Cadena');
+const textoJson = JSON.stringify(taller, null, 2);
+console.log(textoJson);
+console.log('tipo: ', typeof textoJson);
 // TODO: JSON.parse — convierte `textoJson` de vuelta a objeto (guárdalo en `objetoDeVuelta`)
 //       e imprime `objetoDeVuelta.nombre`
+console.log('Transformacion de JSON a objeto');
+const objetoDeVuelta = JSON.parse(textoJson);
+console.log(textoJson);
+console.log('tipo: ', typeof objetoDeVuelta);
+console.log(objetoDeVuelta.nombre);
